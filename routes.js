e2dd2eb33390fff872a71b4c5bfbe719a8bce94c
Eldar <eldar.restigo@gmail.com>
2022-10-19 14:48:44 +0300

    test

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/payload", async function(req, res) {
-    console.log(req.body)
+    console.log(req.body);
 
     // const payload = req.body;
     //
