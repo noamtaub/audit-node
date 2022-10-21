f9086c4958b1198c53deca007c2ed8914f058214
Eldar <eldar.restigo@gmail.com>
2022-10-21 09:07:21 +0300

    capture pull request

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require('core-js/stable');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const cors = require("cors")
+const crypto = require('crypto');
+const puppeteer = require('puppeteer');
 
 mongoose.connect('mongodb://localhost:27017/webhook')
     .then(() => console.log("connected to mongoose"))
@@ -10,6 +12,8 @@ mongoose.connect('mongodb://localhost:27017/webhook')
 
 // App
 const app = express();
+
+
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,10 +30,26 @@ const Ping = mongoose.model('ping', new mongoose.Schema({
     payload: Object
 }))
 
+app.use(bodyParser.json());
+
+const secret = 'ABCD1234';
+const sigHeaderName = 'X-Hub-Signature-256'
+const sigHashAlg = 'sha256';
+
 app.get('/', async (req, res) => {
     res.send(await Ping.find().sort('payload'))
 })
-app.post('/', async (req, res) => {
+app.post('/',verifyPayload, async (req, res) => {
+
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(req.body.pull_request.url);
+
+        page.setViewport({ width: 300, height: 2000 })
+        await page.screenshot({ path: `screenshot${Date.now()}.png` });
+
+        await browser.close();
+
     let ping = new Ping({
         payload: req.body
     })
@@ -38,5 +58,27 @@ app.post('/', async (req, res) => {
         message: ping
     });
 })
-// Server
+
+app.use((err, req, res, next) => {
+    if (err) {
+        console.error(err);
+    }
+    res.status(403).send('Request body was not signed or verification failed');
+});
+
+function verifyPayload(req, res, next) {
+    if (!req.body) {
+        return next('Request body empty')
+    }
+
+    const data = JSON.stringify(req.body);
+    const sig = Buffer.from(req.get(sigHeaderName) || '', 'utf8');
+    const hmac = crypto.createHmac(sigHashAlg, secret);
+    const digest = Buffer.from(`${sigHashAlg}=${hmac.update(data).digest('hex')}`, 'utf8');
+    if (sig.length !== digest.length || !crypto.timingSafeEqual(digest, sig)) {
+        return next(`Request body digest (${digest}) did not match ${sigHeaderName} (${sig})`);
+    }
+    return next()
+}
+
 app.listen(port, () => console.log(`Server running on localhost:${port}`));
